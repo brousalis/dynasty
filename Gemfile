4c4ccd0f7860e2c359b767dc16464fdd957ec96e
Pete Brousalis <pbrousalis@enova.com>
2013-11-27 18:33:16 -0600

    Initial commit

---
+++ Gemfile
@@ -0,0 +1,15 @@
+source 'https://rubygems.org'
+
+gem 'rails', '3.2.11'
+
+gem 'json'
+
+gem 'bootstrap-sass'
+gem 'haml'
+
+group :assets do
+  gem 'sass-rails',   '~> 3.2.3'
+  gem 'uglifier', '>= 1.0.3'
+end
+
+gem 'jquery-rails'
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Redesign::Application.config.secret_token = 'a51479150d8d7df9657be3822d5238894b3e1df40634bfeafebb4d90c898ebc77030074d658c0c59c386828092c88d9dba4ef4606394f4315ce4e742889a9a02'
